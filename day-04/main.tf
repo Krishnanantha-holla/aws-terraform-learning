3188d98d93c1fdc2c3397172e7a94b5286fbda9b
krishnanantha-holla <krishnananthaholla@gmail.com>
2026-03-12 09:20:53 +0530

    minor changes

--- day-04/main.tf
+++ day-04/main.tf
@@ -6,10 +6,9 @@ terraform {
     region = "us-east-1"
     encrypt = true
     use_lockfile = true
-    
   }
 
-required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
@@ -21,6 +20,18 @@ required_providers {
 provider "aws" {
   region = "us-east-1"
 }
+variable "envi" {
+  default = "dev"
+  type    = string
+}
 
 
+#Create a s3 bucket
+resource "aws_s3_bucket" "The-demo-bucket" {
+  bucket = "my-first-bucket-hell-yeah"
 
+  tags = {
+    Name        = "My bucket Here"
+    Environment = var.envi
+  }
+}
